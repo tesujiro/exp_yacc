1fdc84eefe7d5c3133511ae39216ce019bc37585
tesujiro <tesujiro@gmail.com>
2018-06-18 22:08:50 +0900

    change: devide parser and main

--- parser/parser.go
+++ parser/parser.go
@@ -4,26 +4,6 @@ package parser
 
 import __yyfmt__ "fmt"
 
-import (
-	"strconv"
-	"text/scanner"
-)
-
-type Expression interface{}
-type Token struct {
-	token   int
-	literal string
-}
-
-type NumExpr struct {
-	literal string
-}
-type BinOpExpr struct {
-	left     Expression
-	operator rune
-	right    Expression
-}
-
 type yySymType struct {
 	yys   int
 	token Token
@@ -338,50 +318,3 @@ yynewstate:
 	}
 	goto yystack /* stack new state and value */
 }
-
-type Lexer struct {
-	scanner.Scanner
-	Result Expression
-}
-
-func (l *Lexer) Lex(lval *yySymType) int {
-	token := int(l.Scan())
-	if token == scanner.Int {
-		token = NUMBER
-	}
-	lval.token = Token{token: token, literal: l.TokenText()}
-	return token
-}
-
-func (l *Lexer) Error(e string) {
-	panic(e)
-}
-
-func Eval(e Expression) int {
-	switch e.(type) {
-	case BinOpExpr:
-		left := Eval(e.(BinOpExpr).left)
-		right := Eval(e.(BinOpExpr).right)
-		switch e.(BinOpExpr).operator {
-		case '+':
-			return left + right
-		}
-	case NumExpr:
-		num, _ := strconv.Atoi(e.(NumExpr).literal)
-		return num
-	}
-	return 0
-}
-
-func Parse(yylex yyLexer) int {
-	return yyParse(yylex)
-}
-
-/*
-func main() {
-    l := new(Lexer)
-    l.Init(strings.NewReader(os.Args[1]))
-    yyParse(l)
-    fmt.Printf("%#v\n", Eval(l.result))
-}
-*/
