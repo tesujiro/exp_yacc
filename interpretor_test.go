e730122a456eed70e94ab6dd7cd1ead3c2a825dd
tesujiro <tesujiro@gmail.com>
2018-06-28 22:23:44 +0900

    fix: test bug

--- interpretor_test.go
+++ interpretor_test.go
@@ -48,7 +48,7 @@ func TestInterpretor(t *testing.T) {
 		{stdin: "a=1;b=0.1;c=15;(a+b)*c/0.5", stdout: "33"},
 		{stdin: "a=1;if a==1 { a=2 }", stdout: "2"},
 		{stdin: "a=1;if a==1 { a=2 };a", stdout: "2"},
-		{stdin: "a=1;if a==1 { b=2 };b", stdout: "Eval error:unknown symbol 'b'"},
+		{stdin: "a=1;if a==1 { env_test_xx=2 };env_test_xx", stdout: "Eval error:unknown symbol 'env_test_xx'"},
 		{stdin: "a=2;if a==1 { a=2 } else { a=3;b=4 }", stdout: "4"},
 		{stdin: "a=1;b=1;if a==1 { b=2 };b", stdout: "2"},
 	}
